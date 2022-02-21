5ab8e134bceed2bd08df1081b7ce96f66d584089
Павел Пацей <patsey.pavel@gmail.com>
2022-02-21 12:21:08 +0300

    posts/tests/test_forms.py перенес создание картинки в setUpClass

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -17,6 +17,14 @@ class PostFormsTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.small_gif = (
+            b"\x47\x49\x46\x38\x39\x61\x02\x00"
+            b"\x01\x00\x80\x00\x00\x00\x00\x00"
+            b"\xFF\xFF\xFF\x21\xF9\x04\x00\x00"
+            b"\x00\x00\x00\x2C\x00\x00\x00\x00"
+            b"\x02\x00\x01\x00\x00\x02\x02\x0C"
+            b"\x0A\x00\x3B"
+        )
         cls.user = User.objects.create_user(username="testusername")
         cls.group = Group.objects.create(
             title="Test title",
@@ -73,16 +81,10 @@ class PostFormsTest(TestCase):
     def test_post_create_page_creates_new_post_with_image(self):
         """На странице post_create успешно создан пост с картинкой"""
         post_count = Post.objects.count()
-        small_gif = (
-            b"\x47\x49\x46\x38\x39\x61\x02\x00"
-            b"\x01\x00\x80\x00\x00\x00\x00\x00"
-            b"\xFF\xFF\xFF\x21\xF9\x04\x00\x00"
-            b"\x00\x00\x00\x2C\x00\x00\x00\x00"
-            b"\x02\x00\x01\x00\x00\x02\x02\x0C"
-            b"\x0A\x00\x3B"
-        )
         uploaded = SimpleUploadedFile(
-            name="small.gif", content=small_gif, content_type="image/gif"
+            name="small.gif",
+            content=PostFormsTest.small_gif,
+            content_type="image/gif",
         )
         form_data = {
             "text": "test form text",
