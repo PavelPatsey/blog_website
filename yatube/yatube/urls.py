daf4d60a158ef267e0190e7770e6750cabca8c11
Павел Пацей <patsey.pavel@gmail.com>
2022-02-21 15:01:00 +0300

    yatube/urls.py удалил комментарии

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,18 +1,3 @@
-"""yatube URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -30,7 +15,7 @@ handler404 = "core.views.page_not_found"
 handler403 = "core.views.page_forbidden"
 handler500 = "core.views.server_error"
 
-CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
+CSRF_FAILURE_VIEW = "core.views.csrf_failure"
 
 if settings.DEBUG:
     urlpatterns += static(
