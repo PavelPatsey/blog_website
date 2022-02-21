837f785a5d82ff051b0ea8b7b964ab4d8c45a707
Павел Пацей <patsey.pavel@gmail.com>
2022-02-21 12:43:37 +0300

    posts/admin.py применид декоратор @admin.register(Post)

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Comment, Follow, Group, Post
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         "pk",
@@ -17,7 +18,6 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
-admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
 admin.site.register(Comment)
 admin.site.register(Follow)
